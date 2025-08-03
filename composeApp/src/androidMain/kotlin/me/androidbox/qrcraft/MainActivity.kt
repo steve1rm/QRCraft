7b5eaaa7a129ee329f70ef96d9b640919706003e
XDdevv <undefineduser087@gmail.com>
2025-08-03 15:27:50 +0500

    Splash screen implemented for android

--- composeApp/src/androidMain/kotlin/me/androidbox/qrcraft/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/me/androidbox/qrcraft/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+        enableEdgeToEdge()
 
         setContent {
             App()
