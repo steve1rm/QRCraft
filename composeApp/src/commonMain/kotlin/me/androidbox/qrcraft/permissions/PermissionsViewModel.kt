944bb541871ce869b56e4f8f5d85c231f1ed0734
steve <stevekitkat@gmail.com>
2025-08-04 07:09:59 +0100

    Adds ViewModel for handling camera permissions
    This commit introduces `PermissionsViewModel` to manage camera permission requests using the moko-permissions library.
    
    Key changes:
    - Created `PermissionsViewModel` to check and request camera permissions.
    - Updated `composeApp/build.gradle.kts` to include `moko-permissions` and `moko-permissions-compose` dependencies.
    - Added moko version to `gradle/libs.versions.toml`.

---
+++ composeApp/src/commonMain/kotlin/me/androidbox/qrcraft/permissions/PermissionsViewModel.kt
@@ -0,0 +1,48 @@
+package me.androidbox.qrcraft.permissions
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.icerock.moko.permissions.DeniedAlwaysException
+import dev.icerock.moko.permissions.DeniedException
+import dev.icerock.moko.permissions.Permission
+import dev.icerock.moko.permissions.PermissionState
+import dev.icerock.moko.permissions.PermissionsController
+import dev.icerock.moko.permissions.RequestCanceledException
+import kotlinx.coroutines.launch
+
+class PermissionsViewModel(
+    private val permissionsController: PermissionsController
+) : ViewModel() {
+
+    var permissionState by mutableStateOf(PermissionState.NotDetermined)
+        private set
+
+    init {
+        viewModelScope.launch {
+            permissionState = permissionsController.getPermissionState(Permission.CAMERA)
+        }
+    }
+
+    fun provideOrRequestCameraPermission() {
+        viewModelScope.launch {
+            try {
+                permissionsController.providePermission(Permission.CAMERA)
+                permissionState = PermissionState.Granted
+            }
+            catch (exception: DeniedAlwaysException) {
+                permissionState = PermissionState.DeniedAlways
+                exception.printStackTrace()
+            }
+            catch(exception: DeniedException) {
+                permissionState = PermissionState.Denied
+                exception.printStackTrace()
+            }
+            catch (exception: RequestCanceledException) {
+                exception.printStackTrace()
+            }
+        }
+    }
+}
