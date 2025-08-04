26fa718b68d593d0f2c3e3315043308ddfddda55
steve <stevekitkat@gmail.com>
2025-08-04 13:32:10 +0700

    Integrates moko-permissions for camera permission handling
    This commit introduces `moko-permissions` to manage camera permissions within the `ScanningScreen`.

--- composeApp/src/androidMain/kotlin/me/androidbox/qrcraft/presentation/ScanningScreen.kt
+++ composeApp/src/androidMain/kotlin/me/androidbox/qrcraft/presentation/ScanningScreen.kt
@@ -3,16 +3,28 @@
 package me.androidbox.qrcraft.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import dev.icerock.moko.permissions.compose.BindEffect
+import dev.icerock.moko.permissions.compose.rememberPermissionsControllerFactory
+import me.androidbox.qrcraft.permissions.PermissionsViewModel
 
 @Composable
 fun ScanningScreen(
     cameraPreviewViewModel: CameraPreviewViewModel,
+    permissionsViewModel: PermissionsViewModel,
     modifier: Modifier = Modifier
 ) {
+    val factory = rememberPermissionsControllerFactory()
+    val permissionController = remember(factory) {
+        factory.createPermissionsController()
+    }
+
+    BindEffect(permissionController)
+
     val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
 
     if(!cameraPermissionState.status.isGranted) {
