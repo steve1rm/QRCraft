2b22587b580ee11eeaa751fdd870834de1ce46ce
XDdevv <undefineduser087@gmail.com>
2025-08-23 21:02:52 +0500

    Applied full-screen preview for Scanning and removed bottom navigation background for Scanning screen

--- composeApp/src/commonMain/kotlin/me/androidbox/qrcraft/scanning/presentation/ScanningScreen.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/qrcraft/scanning/presentation/ScanningScreen.kt
@@ -1,11 +1,9 @@
 package me.androidbox.qrcraft.scanning.presentation
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
--- composeApp/src/commonMain/kotlin/me/androidbox/qrcraft/scanning/presentation/components/ScanningSurfaceWithCorners.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/qrcraft/scanning/presentation/components/ScanningSurfaceWithCorners.kt
@@ -35,7 +35,6 @@ fun ScanningSurfaceRoundedCorners(
 ) {
     Surface(
         modifier = modifier,
-        shape = RoundedCornerShape(surfaceRadius)
     ) {
         Box(
             modifier = Modifier
